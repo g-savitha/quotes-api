9519d46b0b9de19de16c5748c74606e8b2f8a8c1
Savitha <gsavitha@protonmail.com>
2022-12-07 16:13:25 +0530

    Added new routes and quotes

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const quotes = require('./api/quotes.json');
 app.get('/', (req, res) => {
   res.json(quotes);
 });
+//Get Bhagavadgita Quotes
+app.get('/bg', (req, res) => {
+  res.json(quotes.bg);
+});
+//Get stoic quotes
+app.get('/stoic', (req, res) => {
+  res.json(quotes.stoic);
+});
+// Get APJ Quotes
+app.get('/apj', (req, res) => {
+  res.json(quotes.apj);
+});
 
 app.listen(3000, () => {
   console.log('API listening on port 3000');
